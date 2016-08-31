fb2ad54fdc574cd14a575dba4b16d847b2b81051
niCEnANi <nicenani.iiit@gmail.com>
2016-08-31 01:23:13 -0500

    source

---
+++ Source/Lab1/ScalaDemo/src/ScalaApp.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by nicen on 30-08-2016.
+  */
+import org.apache.spark.{SparkContext, SparkConf}
+
+object ScalaApp {
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir", "D:\\winutils");
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+
+    val sc = new SparkContext(sparkConf)
+
+    val input = sc.textFile("input")
+
+    val wc = input.flatMap(line=>{line.split('.')}).map( line => (line, 1)).cache()
+
+    val output = wc.reduceByKey(_ + _).sortByKey()
+
+    output.saveAsTextFile("output")
+
+    val o = output.collect()
+
+    var s: String = "Sentence:Count & Ascending order by count \n"
+    o.foreach { case (word, count) => {
+
+      s += word + " : " + count + "\n"
+    }}
+    println(s)
+  }
+
+}
